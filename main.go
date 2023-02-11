bf32c942c1320ac44bab8906964a0d4de8308209
David Bourke <davidrbourke@gmail.com>
2023-02-11 21:01:23 +0000

    Adding path

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
  
+    http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "This is home, %q", html.EscapeString(r.URL.Path))
+    })
+
+
+
     log.Fatal(http.ListenAndServe(":8080", nil))
  
 }
